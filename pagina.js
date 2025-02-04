ded72b8321983fef3acc4da2afb75f55e28f1d30
Guilherme oliveira <161101698+okguilherme@users.noreply.github.com>
2025-02-04 19:36:11 -0300

    add_buscarCargos

--- pagina.js
+++ pagina.js
@@ -1,10 +1,10 @@
 const referencia = document.querySelector(".index1");
+let comparador = [];
+let nomesCargos = [];
 
 fetch("detalhamentopessoal.json").then((resposta) => {
     return resposta.json();
 }).then((informacoes) => {
-    let comparador = [];
-    let nomesCargos = [];
 
     // Preenchendo a lista de cargos
     informacoes.data.map((listagem) => {
@@ -22,12 +22,15 @@ fetch("detalhamentopessoal.json").then((resposta) => {
 
         if (busca.length > 0) {
             // Filtra os cargos de acordo com a busca
-            const filtrados = informacoes.data.filter(item => item.Cargo.toLowerCase().includes(busca));
+            const filtrados = nomesCargos.filter(item => item.toLowerCase().includes(busca));
 
             // Exibe os cargos filtrados
             filtrados.forEach(element => {
                 const li = document.createElement("li");
-                li.textContent = element.Cargo;
+                const a = document.createElement("a");
+                a.href = `paginaFuncionario.html`;
+                a.textContent = element;
+                li.appendChild(a);
                 resultado.appendChild(li);  // Adiciona o item ao resultado
             });
         }
---
+++ paginaFuncionario.js
@@ -0,0 +1,12 @@
+{/* <div class="index2" >
+    <p id="Nome"></p>
+    <p id="Competência"></p>
+    <p id="Folha"></p>
+    <p id="Vínculo"></p>
+    <p id="Cargo"></p>
+    <p id="Setor"></p>
+    <p id="Matricula"></p>
+    <p id="Proventos"></p>
+    <p id="Descontos"></p>
+    <p id="Líquido"></p>
+</div> */}
